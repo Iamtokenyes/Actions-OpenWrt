a0d66fe6c88904234238d988e92098f624ecff4c
Iamtokenyes <92936554+Iamtokenyes@users.noreply.github.com>
2022-03-28 15:36:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,13 +16,13 @@
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
-#git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/destan19/OpenAppFilter.git
 git clone  https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone  https://github.com/gSpotx2f/luci-app-cpu-status.git
-#git clone https://github.com/fw876/helloworld.git
+git clone https://github.com/fw876/helloworld.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git
-#git clone https://github.com/aria2/aria2.git
+git clone https://github.com/aria2/aria2.git
 #git clone https://github.com/lisaac/luci-app-dockerman.git
 
 #git clone https://github.com/Iamtokenyes/luci-app-smartdns.git
@@ -34,7 +34,7 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git
 #svn co https://github.com/openwrt/luci/trunk/applications/luci-app-transmission
 #svn co https://github.com/openwrt/packages/trunk/net/transmission
 #svn co https://github.com/openwrt/packages/trunk/net/transmission-web-control
-#svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
+svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
 cd ../../
 
 #sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
