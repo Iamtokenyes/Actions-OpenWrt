b03d028ad7a24898d77685c44502db10a0a223f3
Iamtokenyes <92936554+Iamtokenyes@users.noreply.github.com>
2022-03-28 15:10:54 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,15 +13,15 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-#cd package
-#mkdir openwrt-packages
-#cd openwrt-packages
+cd package
+mkdir openwrt-packages
+cd openwrt-packages
 #git clone https://github.com/destan19/OpenAppFilter.git
-#git clone  https://github.com/rufengsuixing/luci-app-adguardhome.git
-#git clone  https://github.com/gSpotx2f/luci-app-cpu-status.git
+git clone  https://github.com/rufengsuixing/luci-app-adguardhome.git
+git clone  https://github.com/gSpotx2f/luci-app-cpu-status.git
 #git clone https://github.com/fw876/helloworld.git
-#git clone https://github.com/jerrykuku/lua-maxminddb.git
-#git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
 #git clone https://github.com/aria2/aria2.git
 #git clone https://github.com/lisaac/luci-app-dockerman.git
 
@@ -35,6 +35,6 @@
 #svn co https://github.com/openwrt/packages/trunk/net/transmission
 #svn co https://github.com/openwrt/packages/trunk/net/transmission-web-control
 #svn co https://github.com/sirpdboy/netspeedtest/trunk/luci-app-netspeedtest
-#cd ../../
+cd ../../
 
 #sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
